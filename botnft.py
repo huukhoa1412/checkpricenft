f0b20f3e7a5fac66a1de4c0978dc0819d96c641f
huukhoa1412 <59795495+huukhoa1412@users.noreply.github.com>
2022-04-13 02:38:38 +0700

    Update botnft.py

--- botnft.py
+++ botnft.py
@@ -142,7 +142,7 @@ def action(msg):
         print(f"S2: R {price_the_r2} {donvi_the_r2} | SR {price_the_sr2} {donvi_the_sr2}| SSR {price_the_ssr2} {donvi_the_ssr2}| Box {price_box_s2} {donvi_box_r2}")
         print(f"S3: R {price_the_r3} {donvi_the_r3} | SR {price_the_sr3}  {donvi_the_sr3}| SSR {price_the_ssr3} {donvi_the_ssr3}| Box {price_box_s3} {donvi_box_r3}") 
 
-        telegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} {donvi_the_r1}| SR {price_the_sr1} {donvi_the_sr1}| SSR {price_the_ssr1} {donvi_the_ssr1}| Box {price_box_s1} {donvi_box_r1} \nS2: R {price_the_r2} {donvi_the_r2}| SR {price_the_sr2} {donvi_the_sr2}| SSR {price_the_ssr2} {donvi_the_ssr2}| Box {price_box_s2} {donvi_box_r2} \nS3: R {price_the_r3} {donvi_the_r3}| SR {price_the_sr3} {donvi_the_sr3}| SSR {price_the_ssr3} {price_the_ssr3}| Box {price_box_s3} {price_box_s3} {donvi_box_r3}\n")
+        telegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} {donvi_the_r1}| SR {price_the_sr1} {donvi_the_sr1}| SSR {price_the_ssr1} {donvi_the_ssr1}| Box {price_box_s1} {donvi_box_r1} \nS2: R {price_the_r2} {donvi_the_r2}| SR {price_the_sr2} {donvi_the_sr2}| SSR {price_the_ssr2} {donvi_the_ssr2}| Box {price_box_s2} {donvi_box_r2} \nS3: R {price_the_r3} {donvi_the_r3}| SR {price_the_sr3} {donvi_the_sr3}| SSR {price_the_ssr3} {donvi_the_ssr3}| Box {price_box_s3} {donvi_box_r3}\n")
 
 
 telegram_bot = telepot.Bot('5235784128:AAEd5gY8AHcmNH1adsMCzM8QWFJZY2KUrAc')
