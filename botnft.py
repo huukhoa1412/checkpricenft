c806c57cce75b390276ec017c84a382b32af603b
huukhoa1412 <59795495+huukhoa1412@users.noreply.github.com>
2022-04-13 17:13:57 +0700

    Update botnft.py

--- botnft.py
+++ botnft.py
@@ -6,11 +6,14 @@ from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
 
-def send_req(trang,id,seial):
+def send_req(id,seial):
     url = 'https://www.binance.com/bapi/nft/v1/public/nft/market-mystery/mystery-list'
     pre_result = []
-    req_data = {"page": trang,
-                    "size": 60,
+
+    for i in range(1, 4):
+
+        req_data = {"page": i,
+                    "size": 100,
                     "params":
                         {
                             "keyword": "",
@@ -22,18 +25,19 @@ def send_req(trang,id,seial):
                         }
                     }
 
-    headers = {
+        headers = {
             'Content-type': 'application/json',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/94.0.4606.85 YaBrowser/21.11.1.932 Yowser/2.5 Safari/537.36'
-    }
-    r = requests.post(url, data=json.dumps(req_data), headers=headers)
-    pre_result.append(r.json()['data']['data'])
+        }
+        r = requests.post(url, data=json.dumps(req_data), headers=headers)
+        pre_result.append(r.json()['data']['data'])
 
     result = []
     for i in pre_result:
         for j in i:
             result.append(j)
+
     return result
 
 def action(msg):
@@ -45,105 +49,82 @@ def action(msg):
 
     # markup = ReplyKeyboardMarkup(keyboard=[['Time', KeyboardButton(text='NewKey')],["File", "Audio"]])
     if command == '/n' or command == '/n@xabang_bot' or command == '/nft' or command == '/nfts':
-    #Box S1
-        #R box S1
-        items_the_r1 = send_req(1,3,109191979564517376)
-        items_box_s1 = send_req(1,2,109191979564517376)
-        #gia
-        price_the_r1 = items_the_r1[0]['amount']
-        #donvi
-        donvi_the_r1 = items_the_r1[0]['currency']
-        #gia
+    items_the_s1 = send_req(3,109191979564517376)
+        items_box_s1 = send_req(2,109191979564517376)
         price_box_s1 = items_box_s1[0]['amount']
-        #donvi
-        donvi_box_r1 = items_box_s1[0]['currency']
-        #RS box S1
-        items_the_sr1 = send_req(2,3,109191979564517376)
-        loai_the_sr1 = 2
-        i = 0
-        while (loai_the_sr1 > 1):
-            i = i + 1   
-            loai_the_sr1= items_the_sr1[i]['rarity']
-        #print(f"sr1 {i}")     
-        donvi_the_sr1 = items_the_sr1[i]['currency']
-        price_the_sr1 = items_the_sr1[i]['amount']
-        #RSS box S1
-        items_the_ssr1 = send_req(2,3,109191979564517376)
-        loai_the_ssr1 = 1
-        j = 20
-        while (loai_the_ssr1 > 0):
-            j = j + 1 
-            loai_the_ssr1= items_the_ssr1[j]['rarity']
-        #print(f"ssr1 {j}")     
-        donvi_the_ssr1 = items_the_ssr1[j]['currency']
-        price_the_ssr1 = items_the_ssr1[j]['amount']
-        #SSR box S1            
-    #Box S2
-        #R box S2
-        items_the_r2 = send_req(1,3,155499473454738432)
-        items_box_s2 = send_req(1,2,155499473454738432)
-        #gia
-        price_the_r2 = items_the_r2[0]['amount']
-        #donvi
-        donvi_the_r2 = items_the_r2[0]['currency']
-        #gia
+        items_the_s2 = send_req(3,155499473454738432)
+        items_box_s2 = send_req(2,155499473454738432)
         price_box_s2 = items_box_s2[0]['amount']
-        #donvi
-        donvi_box_r2 = items_box_s2[0]['currency']
-        #RS box S2
-        items_the_sr2 = send_req(2,3,155499473454738432)
-        loai_the_sr2 = 2
-        i = 0
-        while (loai_the_sr2 > 1):
-            i = i + 1 
-            loai_the_sr2= items_the_sr2[i]['rarity']
-        #print(f"sr2 {i}")    
-        donvi_the_sr2 = items_the_sr2[i]['currency']
-        price_the_sr2 = items_the_sr2[i]['amount']
-        #RSS box S2
-        items_the_ssr2 = send_req(2,3,155499473454738432)
-        loai_the_ssr2 = 1
-        j = 20
-        while (loai_the_ssr2 > 0):
-            j = j + 1
-            loai_the_ssr2 = items_the_ssr2[j]['rarity']
-        #print(f"ssr2 {j}")    
-        donvi_the_ssr2 = items_the_ssr2[j]['currency']
-        price_the_ssr2 = items_the_ssr2[j]['amount']
-        #SSR box S2
-      #Box S3
-        #R box S2
-        items_the_r3 = send_req(1,3,180885755495849984)
-        items_box_s3 = send_req(1,2,180885755495849984)
-        #gia
-        price_the_r3 = items_the_r3[0]['amount']
-        #donvi
-        donvi_the_r3 = items_the_r3[0]['currency']
-        #gia
+        items_the_s3 = send_req(3,180885755495849984)
+        items_box_s3 = send_req(2,180885755495849984)
         price_box_s3 = items_box_s3[0]['amount']
-        #donvi
-        donvi_box_r3 = items_box_s3[0]['currency']
-        #RS box S2
-        items_the_sr3 = send_req(4,3,180885755495849984)
-        loai_the_sr3 = 2
-        i = 0
-        while (loai_the_sr3 > 1):
-            i = i + 1
-            loai_the_sr3 = items_the_sr3[i]['rarity']
-        #print(f"sr3 {i}")    
-        donvi_the_sr3 = items_the_sr3[i]['currency']
-        price_the_sr3 = items_the_sr3[i]['amount']
-        #RSS box S2
-        items_the_ssr3 = send_req(4,3,180885755495849984)
-        loai_the_ssr3 = 1
-        j = 20
-        while (loai_the_ssr3 > 0):
-            j = j + 1    
-            loai_the_ssr3= items_the_ssr3[j]['rarity']
-        #print(f"ssr3 {j}")    
-        donvi_the_ssr3 = items_the_ssr3[j]['currency']
-        price_the_ssr3 = items_the_ssr3[j]['amount']
-        #SSR box S2         
+        total_s1 = 0
+        total_sr1 = 0
+        check_sr1 = 0
+        total_ssr1 = 0
+        check_ssr1 = 0
+        total_s2 = 0
+        total_sr2 = 0
+        check_sr2 = 0
+        total_ssr2 = 0
+        check_ssr2 = 0
+        total_s3 = 0
+        total_sr3 = 0
+        check_sr3 = 0
+        total_ssr3 = 0
+        check_ssr3 = 0
+        for i in range(len(items_the_s1)):
+            price_the_r1 = items_the_s1[0]['amount']
+            loai = items_the_s1[i]['rarity']
+            if (loai == 2):
+                total_s1 += 1 
+            if (loai == 1):
+                total_sr1 += 1
+                if check_sr1 == 0:
+                    price_the_sr1 = items_the_s1[i]['amount']
+                    check_sr1 = 1
+            if (loai == 0):
+                total_ssr1 += 1
+                if check_ssr1 == 0:
+                    price_the_ssr1 = items_the_s1[i]['amount']
+                    check_ssr1 = 1          
+        for i in range(len(items_the_s2)):
+            price_the_r2 = items_the_s2[0]['amount']
+            loai =  items_the_s2[i]['rarity']
+            if (loai == 2):
+                total_s2 += 1 
+            if (loai == 1):
+                total_sr2 += 1
+                if check_sr2 == 0:
+                    price_the_sr2 = items_the_s2[i]['amount']
+                    check_sr2 = 1
+            if (loai == 0):
+                total_ssr2 += 1
+                if check_ssr2 == 0:
+                    price_the_ssr2 = items_the_s2[i]['amount']
+                    check_ssr2 = 1
+        for i in range(len(items_the_s3)):
+            price_the_r3 = items_the_s3[0]['amount']
+            loai =  items_the_s3[i]['rarity']
+            if (loai == 2):
+                total_s3 += 1 
+            if (loai == 1):
+                total_sr3 += 1
+                if check_sr3 == 0:
+                    price_the_sr3 = items_the_s3[i]['amount']
+                    check_sr3 = 1
+            if (loai == 0):
+                total_ssr3 += 1
+                if check_ssr3 == 0:
+                    price_the_ssr3 = items_the_s3[i]['amount']
+                    check_ssr3 = 1          
+        #print(f'{total_s1} - {total_sr1} - {total_ssr1}')
+        #print(f"S1: R {price_the_r1} | SR {price_the_sr1} | SSR {price_the_ssr1} | Box {price_box_s1} ")
+        #print(f'{total_s2} - {total_sr2} - {total_ssr2}')
+        #print(f"S2: R {price_the_r2} | SR {price_the_sr2} | SSR {price_the_ssr2} | Box {price_box_s2} ")
+        #print(f'{total_s3} - {total_sr3} - {total_ssr3}')
+        #print(f"S3: R {price_the_r3} | SR {price_the_sr3} | SSR {price_the_ssr3} | Box {price_box_s3} ")
+          
         print("OK")
         #elegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} {donvi_the_r1}| SR {price_the_sr1} {donvi_the_sr1}| SSR {price_the_ssr1} {donvi_the_ssr1}| Box {price_box_s1} {donvi_box_r1} \nS2: R {price_the_r2} {donvi_the_r2}| SR {price_the_sr2} {donvi_the_sr2}| SSR {price_the_ssr2} {donvi_the_ssr2}| Box {price_box_s2} {donvi_box_r2} \nS3: R {price_the_r3} {donvi_the_r3}| SR {price_the_sr3} {donvi_the_sr3}| SSR {price_the_ssr3} {donvi_the_ssr3}| Box {price_box_s3} {donvi_box_r3}\n")
         telegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} | SR {price_the_sr1} | SSR {price_the_ssr1} | Box {price_box_s1} \nS2: R {price_the_r2} | SR {price_the_sr2} | SSR {price_the_ssr2} | Box {price_box_s2} \nS3: R {price_the_r3} | SR {price_the_sr3} | SSR {price_the_ssr3} | Box {price_box_s3}\n")
