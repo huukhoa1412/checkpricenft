5f9c77f89755c21503790b9c2231d9c43120cc27
huukhoa1412 <59795495+huukhoa1412@users.noreply.github.com>
2022-08-07 02:57:46 +0700

    Update botnft.py

--- botnft.py
+++ botnft.py
@@ -1,81 +1,28 @@
 import json
-import os
 import time
 import requests
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
-
-
-# https://www.binance.com/en/nft/search-result?serialNo=109191979564517376&nftType=3&tradeType=0&rarity=2&tab=mystery-box&keyword=topgoal&order=amount_sort%401
 
 def send_req(seial,loai):
     url = 'https://www.binance.com/bapi/nft/v1/public/nft/market-mystery/mystery-list'
     pre_result = []
-
-    for i in range(1, 2):
-
-        req_data = {"page": i,
-                    "size": 16,
-                    "params":
-                        {
-                            "keyword": "topgoal",
-                            "nftType": "3",
-                            "orderBy": "amount_sort",
-                            "orderType": "1",
-                            "rarity": loai,
-                            "serialNo": [seial],
-                            "tradeType": "0"
-                        }
-                    }
-
-        headers = {
-            'Content-type': 'application/json',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/94.0.4606.85 YaBrowser/21.11.1.932 Yowser/2.5 Safari/537.36'
-        }
-        r = requests.post(url, data=json.dumps(req_data), headers=headers)
-        pre_result.append(r.json()['data']['data'])
-    time.sleep(0.5)    
-    result = []
-    for i in pre_result:
+	@@ -72,70 +73,91 @@ def send_req_box(seial):
         for j in i:
             result.append(j)
     return result  
 
-def send_req_box(seial):
-    url = 'https://www.binance.com/bapi/nft/v1/public/nft/market-mystery/mystery-list'
-    pre_result = []
+def action(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print('Chat:', content_type, chat_type, chat_id)
+    command = msg['text']
 
-    for i in range(1, 2):
+    print('Received: %s' % command)
 
-        req_data = {"page": i,
-                    "size": 16,
-                    "params":
-                        {
-                            "keyword": "topgoal",
-                            "nftType": "2",
-                            "orderBy": "amount_sort",
-                            "orderType": "1",
-                            "serialNo": [seial],
-                            "tradeType": "0"
-                        }
-                    }
-
-        headers = {
-            'Content-type': 'application/json',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/94.0.4606.85 YaBrowser/21.11.1.932 Yowser/2.5 Safari/537.36'
-        }
-        r = requests.post(url, data=json.dumps(req_data), headers=headers)
-        pre_result.append(r.json()['data']['data'])
-   
-    result = []
-    for i in pre_result:
-        for j in i:
-            result.append(j)
-    return result  
-    
-def main():
-    while True:
+    # markup = ReplyKeyboardMarkup(keyboard=[['Time', KeyboardButton(text='NewKey')],["File", "Audio"]])
+    if command == '/n' or command == '/n@TopGoalCheckerPriceBot' or command == '/nft' or command == '/nfts':
         #Box S1
         items_box_s1 = send_req_box(109191979564517376)
         try:
@@ -150,14 +97,23 @@ def main():
             price_the_ssr3 = items_the_ssr3[0]['amount']
         except IndexError:
             price_the_ssr3 = 0
-            
+
         #print(f'{total_s1} - {total_sr1} - {total_ssr1}')
-        print(f"S1: R {price_the_r1} | SR {price_the_sr1} | SSR {price_the_ssr1} | Box {price_box_s1} ")
+        #print(f"S1: R {price_the_r1} | SR {price_the_sr1} | SSR {price_the_ssr1} | Box {price_box_s1} ")
         #print(f'{total_s2} - {total_sr2} - {total_ssr2}')
-        print(f"S2: R {price_the_r2} | SR {price_the_sr2} | SSR {price_the_ssr2} | Box {price_box_s2} ")
+        #print(f"S2: R {price_the_r2} | SR {price_the_sr2} | SSR {price_the_ssr2} | Box {price_box_s2} ")
         #print(f'{total_s3} - {total_sr3} - {total_ssr3}')
-        print(f"S3: R {price_the_r3} | SR {price_the_sr3} | SSR {price_the_ssr3} | Box {price_box_s3} ")
+        #print(f"S3: R {price_the_r3} | SR {price_the_sr3} | SSR {price_the_ssr3} | Box {price_box_s3} ")
 
+        print("OK")
+        #elegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} {donvi_the_r1}| SR {price_the_sr1} {donvi_the_sr1}| SSR {price_the_ssr1} {donvi_the_ssr1}| Box {price_box_s1} {donvi_box_r1} \nS2: R {price_the_r2} {donvi_the_r2}| SR {price_the_sr2} {donvi_the_sr2}| SSR {price_the_ssr2} {donvi_the_ssr2}| Box {price_box_s2} {donvi_box_r2} \nS3: R {price_the_r3} {donvi_the_r3}| SR {price_the_sr3} {donvi_the_sr3}| SSR {price_the_ssr3} {donvi_the_ssr3}| Box {price_box_s3} {donvi_box_r3}\n")
+        telegram_bot.sendMessage (chat_id, f"S1: R {price_the_r1} | SR {price_the_sr1} | SSR {price_the_ssr1} | Box {price_box_s1} \nS2: R {price_the_r2} | SR {price_the_sr2} | SSR {price_the_ssr2} | Box {price_box_s2} \nS3: R {price_the_r3} | SR {price_the_sr3} | SSR {price_the_ssr3} | Box {price_box_s3}\n")
 
-if __name__ == '__main__':
-    main()
+telegram_bot = telepot.Bot('5395216210:AAEKJynIsSMMXuike2rdaKjLdDyQMFkJ_ig')
+print((telegram_bot.getMe()))
+
+MessageLoop(telegram_bot, action).run_as_thread()
+print('Up and Running....')
+
+while 1:
+    time.sleep(0)
